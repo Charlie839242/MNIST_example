c4c78fae562b302e10a908948a30de7fae98fff7
Charlie839242 <122248300@qq.com>
2022-02-03 22:44:50 +0800

    configuration

---
+++ conf.py
@@ -0,0 +1,12 @@
+train_images_idx3_ubyte_file = './dataset/MNIST/raw/train-images-idx3-ubyte'            # train data
+train_labels_idx1_ubyte_file = './dataset/MNIST/raw/train-labels-idx1-ubyte'            # train label
+test_images_idx3_ubyte_file = './dataset/MNIST/raw/t10k-images-idx3-ubyte'              # test data
+test_labels_idx1_ubyte_file = './dataset/MNIST/raw/t10k-labels-idx1-ubyte'              # test label
+
+n_epochs = 4
+batch_size_train = 64
+batch_size_test = 1000
+learning_rate = 0.01
+momentum = 0.5
+log_interval = 10
+random_seed = 1
